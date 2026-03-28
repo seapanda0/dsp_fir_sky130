a5a3f4c0c6bf8cbea9e63f06ddddd592b9138eb3
seapanda0 <1211112330@student.mmu.edu.my>
2026-03-28 17:54:05 +0800

    implemented two stage version

--- src/dsp_fir.v
+++ src/dsp_fir.v
@@ -6,124 +6,170 @@ module dsp_fir(
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    // localparam R = 4'd0;  // Reset state
-    // localparam C0 = 4'd1; // First coeffcient
-    // localparam C1 = 4'd2; //
-    // localparam C2 = 4'd3; //
-    // localparam C3 = 4'd4;
-    // localparam C4 = 4'd5;
-    // localparam C5 = 4'd6;
-    // localparam C6 = 4'd7;
-    // localparam C7 = 4'd8;
-    // localparam M = 4'd7;  // First math stage
+    localparam M1 = 1'd0;  // Reset state
+    localparam M2 = 1'd1;  // First coeffcient
+    // localparam L  = 4'd2;  // Loading stage
 
-    // FSM Disabled
-
-    // reg [3:0] state, next_state;
-
-    // // FSM Switching
-    // always @(*) begin
-    //     if (!rst_n) begin
-    //         next_state <= R;
-    //     end
-    //     else begin
-    //         case (state)
-    //             R : next_state <= C0; 
-    //             C0 : next_state <= C1;
-    //             C1 : next_state <= C2;
-    //             C2 : next_state <= C3;
-    //             C3 : next_state <= C4;
-    //             C4 : next_state <= C5;
-    //             C5 : next_state <= C6;
-    //             C6 : next_state <= C7;
-    //             C7 : next_state <= M;
-    //             default: next_state <= R; 
-    //         endcase 
-    //     end
-    // end
-
-    // always @(posedge clk) begin
-    //     if(!rst_n) begin
-    //         state <= R;
-    //     end
-    //     else begin
-    //         state <= next_state;
-    //     end
-    // end
+    reg state_q, state_d;
 
     // FILTER COEFFICIENTS
     // Register for 8 x 8 bit filter coefficients 
     reg signed [7:0] fir_coeff [0:7];
+    reg signed [7:0] data_pipeline[0:7]; //input data shift register
 
-    // Load the coefficients when mode = 1
     integer  i;
+
     always @(posedge clk) begin
         if (!rst_n) begin
+
+            state_q <= M1; // Reset state back to M1
+
             for(i=0; i<8; i=i+1) begin
                 fir_coeff[i] <= 8'b0;
+                data_pipeline[i] <= 8'sd0;
             end
         end
-        else if (mode == 1'b1) begin
-            fir_coeff[0] <= data_in;
-            fir_coeff[1] <= fir_coeff[0];
-            fir_coeff[2] <= fir_coeff[1];
-            fir_coeff[3] <= fir_coeff[2];
-            fir_coeff[4] <= fir_coeff[3];
-            fir_coeff[5] <= fir_coeff[4];
-            fir_coeff[6] <= fir_coeff[5];
-            fir_coeff[7] <= fir_coeff[6];
-        end
+        else begin
+            if (mode == 1'b1) begin // Load the coefficients when mode = 1
+                fir_coeff[0] <= data_in;
+                fir_coeff[1] <= fir_coeff[0];
+                fir_coeff[2] <= fir_coeff[1];
+                fir_coeff[3] <= fir_coeff[2];
+                fir_coeff[4] <= fir_coeff[3];
+                fir_coeff[5] <= fir_coeff[4];
+                fir_coeff[6] <= fir_coeff[5];
+                fir_coeff[7] <= fir_coeff[6];
+                
+                // Let the filter start fresh from mode switch
+                state_q <= M1;
+                result_adder_q <= 19'b0;
+
+            end 
+            else begin  // Shift data when mode = 0
+                data_pipeline[0] <= data_in;
+                data_pipeline[1] <= data_pipeline[0];
+                data_pipeline[2] <= data_pipeline[1];
+                data_pipeline[3] <= data_pipeline[2];
+                data_pipeline[4] <= data_pipeline[3];
+                data_pipeline[5] <= data_pipeline[4];
+                data_pipeline[6] <= data_pipeline[5];
+                data_pipeline[7] <= data_pipeline[6];
+
+                state_q <= state_d;
+                result_adder_q <= result_adder_d;
+            end
+        end 
     end
 
-    // DATA PIPELINE & SHIFTING
-    reg signed [7:0] data_pipeline[0:7]; //input data shift register
-    integer  j;
-    always @(posedge clk) begin 
-        if(!rst_n) begin 
-            for (j = 0; j < 8; j = j + 1) begin
-                data_pipeline[j] <= 8'sd0;
+    reg signed [7:0] data_pipeline_split [0:3];
+    reg signed [7:0] fir_coefficient_split [0:3];
+    reg [18:0] carry_over;
+    reg [18:0] result_adder_q, result_adder_d;
+
+    reg output_valid;
+
+    wire [15:0] result_partial [0:3];
+
+    // Combinational logics
+    always @(*) begin
+        case (state_q)
+            M1 : begin
+                state_d = M2;
+
+                output_valid = 1'b0;
+
+                data_pipeline_split[0] = data_pipeline[0];
+                data_pipeline_split[1] = data_pipeline[1];
+                data_pipeline_split[2] = data_pipeline[2];
+                data_pipeline_split[3] = data_pipeline[3];
+
+                fir_coefficient_split[0] = fir_coeff[0];
+                fir_coefficient_split[1] = fir_coeff[1];
+                fir_coefficient_split[2] = fir_coeff[2];
+                fir_coefficient_split[3] = fir_coeff[3];
+
+                carry_over = 19'b0;
             end
-        end else if (mode == 1'b0) begin //only shift data when in run mode
-            data_pipeline[0] <= data_in;
-            data_pipeline[1] <= data_pipeline[0];
-            data_pipeline[2] <= data_pipeline[1];
-            data_pipeline[3] <= data_pipeline[2];
-            data_pipeline[4] <= data_pipeline[3];
-            data_pipeline[5] <= data_pipeline[4];
-            data_pipeline[6] <= data_pipeline[5];
-            data_pipeline[7] <= data_pipeline[6];
-        end
+            M2 : begin
+                state_d = M1;
+
+                output_valid = 1'b1;
+                
+                data_pipeline_split[0] = data_pipeline[4];
+                data_pipeline_split[1] = data_pipeline[5];
+                data_pipeline_split[2] = data_pipeline[6];
+                data_pipeline_split[3] = data_pipeline[7];
+
+                fir_coefficient_split[0] = fir_coeff[4];
+                fir_coefficient_split[1] = fir_coeff[5];
+                fir_coefficient_split[2] = fir_coeff[6];
+                fir_coefficient_split[3] = fir_coeff[7];
+
+                carry_over = result_adder_q;
+            end 
+        endcase
     end
 
-    // DSP FILTER
-    reg signed [15:0] mult_reg [0:7]; //multiplying 8bit * 8bit gives a 16 bit result
-    integer  k;
-    integer  l;
-    always @(posedge clk)begin 
-        if (!rst_n)begin 
-            for (k = 0; k < 8; k = k + 1) begin
-                mult_reg[k] <= 16'sd0;
-            end
-        end else if (mode==1'b0)begin //perform multiplication and store in a register immediately
-            for (l = 0; l < 8; l = l + 1) begin
-                mult_reg[l] <= data_pipeline[l] * fir_coeff[l]; 
-            end
-        end
-    end
-    
-    wire signed [18:0] sum; //summing eight 16-bit numbers requires 3 extra bits to prevent overflow
-    
-    assign sum = mult_reg[0] + mult_reg[1] + 
-                 mult_reg[2] + mult_reg[3] + 
-                 mult_reg[4] + mult_reg[5] + 
-                 mult_reg[6] + mult_reg[7];
-                 
-    //scale the output, drop the lower 8 bits and take the next 8 bits
-    
-    assign data_out=sum[15:8];
+    assign data_out= output_valid ? result_adder_q[15:8] : 8'b0;
+
+
+    multiplier_x4 m1 (
+        .a0(data_pipeline_split[0]),
+        .a1(data_pipeline_split[1]),
+        .a2(data_pipeline_split[2]),
+        .a3(data_pipeline_split[3]),
+        .b0(fir_coefficient_split[0]),
+        .b1(fir_coefficient_split[1]),
+        .b2(fir_coefficient_split[2]),
+        .b3(fir_coefficient_split[3]),
+        .out0(result_partial[0]),
+        .out1(result_partial[1]),
+        .out2(result_partial[2]),
+        .out3(result_partial[3])
+    );
+
+    adder_x5 m2 (
+        .in0(result_partial[0]),
+        .in1(result_partial[1]),
+        .in2(result_partial[2]),
+        .in3(result_partial[3]),
+        .carry_over(carry_over),
+        .out(result_adder_d)
+    );
 
     // TEMPORARY
     // Prevent registers from be optimized out
     // assign data_out = fir_coeff[data_in[2:0]];
 
+endmodule
+
+module multiplier_x4(
+    input  wire signed [7:0] a0,
+    input  wire signed [7:0] a1,
+    input  wire signed [7:0] a2,
+    input  wire signed [7:0] a3,
+    input  wire signed [7:0] b0,
+    input  wire signed [7:0] b1,
+    input  wire signed [7:0] b2,
+    input  wire signed [7:0] b3,
+    output wire signed [15:0] out0,
+    output wire signed [15:0] out1,
+    output wire signed [15:0] out2,
+    output wire signed [15:0] out3
+);
+    assign out0 = a0*b0;
+    assign out1 = a1*b1;
+    assign out2 = a2*b2;
+    assign out3 = a3*b3;
+endmodule
+
+module adder_x5 (
+    input  wire signed [15:0] in0,
+    input  wire signed [15:0] in1,
+    input  wire signed [15:0] in2,
+    input  wire signed [15:0] in3,
+    input  wire signed [18:0] carry_over,
+    output wire signed [18:0] out
+);
+    assign out = in0 + in1 + in2 + in3 + carry_over;
 endmodule
